b27350e287c9e54105dacd61eaada56e39f58bb8
David Whitters <whitterd@mail.gvsu.edu>
2017-11-23 10:40:01 -0500

    Removed clap sensing

--- main.c
+++ main.c
@@ -12,11 +12,6 @@
 #include "Peripherals.h"
 #include "Timers.h"
 
-/* 1.5 Volts */
-#define CLAP_THRESH  (ADC_ONE_VOLT)
-/* 0.5 Volts */
-#define LOW_THRESH  (ADC_QTR_VOLT)
-
 #define BEAT_BUFF_LENGTH ((uint16_t)300u)
 /** Voltage spikes must be at least 1.5V to be considered a beat. */
 #define BEAT_THRESH (ADC_ONE_VOLT + ADC_QTR_VOLT)
@@ -52,29 +47,11 @@
 #define MIC_POW_O       (LATBbits.LATB4)
 
 /* Function prototypes */
-void FindPeak(uint16_t low_thresh, uint16_t high_thresh);
-void SenseClaps(void);
 void ProcessPeriod(void);
 void SenseBeats(void);
 void SetColor(void);
 void ChooseColor(void);
 void FadeColor(void);
-
-/*
-    The two main states of the system. Searches for two consecutive claps within 3/4 of a second
-    during the clap state. The system then enters the beat state. The beat state searches for
-    voltage spikes (beats), and changes the PWM of the LEDs accordingly.
-*/
-typedef enum {
-    CLAP_STATE = 1u,
-    BEAT_STATE
-} STATE_t;
-
-/* Used to set Timer_State when sensing claps. NEW_PERIOD is the default state. */
-typedef enum {
-    SAME_PERIOD = 1u,
-    NEW_PERIOD
-} PERIOD_t;
 
 typedef enum {
     RED = 0u,
@@ -96,14 +73,6 @@ struct Color {
 void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void);
 void __attribute__((__interrupt__, no_auto_psv)) _T2Interrupt(void);
 
-/* The state that the sensor is currently in */
-STATE_t Current_State = CLAP_STATE;
-/* The number of claps that occur in a given time segment */
-uint8_t Clap_Count = 0u;
-/* Set to one when searching for spike, set to zero when searching for valley */
-uint8_t Count_Claps;
-/* State of the timer. Set to NEW_PERIOD every Timer2 interrupt. */
-PERIOD_t Timer_State = NEW_PERIOD;
 /** Time between fading the LEDs */
 uint16_t Fade_Time = 0u;
 
@@ -117,8 +86,8 @@ uint8_t Peak_Count = 0u;
 uint16_t Org_Adc_Val = 0u;
 /** Number used to determine the color and state of the RGB LEDs */
 uint16_t Led_Count = 0u;
-/** The three largest peaks within the last 21 seconds 
-      VAL    PERIOD 
+/** The three largest peaks within the last 21 seconds
+      VAL    PERIOD
     |  X    |  X   |
     |  X    |  X   |
     |  X    |  X   |
@@ -144,17 +113,8 @@ int main(int argc, char** argv) {
     // Disable interrupt nesting
     INTCON1bits.NSTDIS = 1u;
 
+    Timers_T1_SetInt();
     Timers_T2_SetInt();
-
-    BTN_POW_TRIS = 0u;      // Set RA1 to output
-    BTN_POW_O = 1u;         // Set RA1 pin to HIGH
-    
-    // Inverse logic to turn on each module
-    MIC_POW_TRIS = 0u;
-    MIC_POW_O = 0u;
-    
-    AUX_POW_TRIS = 0u;
-    AUX_POW_O = 1u;
 
     Peripherals_EnableInterrupts();
     Timers_Start();
@@ -162,26 +122,15 @@ int main(int argc, char** argv) {
     Curr_Color.Red = 100;
     Curr_Color.Grn = 0u;
     Curr_Color.Blu = 0;
-    
+
     Peripherals_PWM_SetDC(RED_CHANNEL, Curr_Color.Red);
     Peripherals_PWM_SetDC(GRN_CHANNEL, Curr_Color.Grn);
     Peripherals_PWM_SetDC(BLU_CHANNEL, Curr_Color.Blu);
 
     while(1u)
     {
-        switch(Current_State)
-        {
-            case CLAP_STATE:
-                SenseClaps();
-                break;
-            case BEAT_STATE:
-                SenseBeats();
-                break;
-            default:
-                Current_State = CLAP_STATE;
-                break;
-        }
-        
+        SenseBeats();
+
         if(T2_Rollover == 1u)
         {
             ProcessPeriod();
@@ -202,42 +151,10 @@ void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void)
 
 void __attribute__((__interrupt__, no_auto_psv)) _T2Interrupt(void)
 {
-    if((Clap_Count >= 1u) && (Current_State == CLAP_STATE))
-    {
-        Current_State = BEAT_STATE;
-        Timers_T1_SetInt();
-        BTN_POW_O = 0u;     // Turn off RA1
-        MIC_POW_O = 1u;
-        AUX_POW_O = 0u;
-    }
-    
-    if(Current_State == BEAT_STATE)
-    {
-        T2_Rollover = 1u;
-    }
-    else
-    {
-        Timer_State = NEW_PERIOD;
-        Clap_Count = 0u;
-    }
+    T2_Rollover = 1u;
 
     IFS0bits.T2IF = 0u;        // Clear flag
 }
-
-//void __attribute__((__interrupt__, no_auto_psv)) _IC2Interrupt(void)
-//{
-//    Clap_Count++;
-//
-//    IFS0bits.IC2IF = 0u;        // Clear flag
-//}
-
-/*
-void _ISR _T2Interrupt(void)
-{
-    LED_O ^= 1u;
-    IFS0bits.T1IF = 0u;         // Clear Timer1 Interrupt Flag
-}
-*/
 
 void ProcessPeriod(void)
 {
@@ -245,7 +162,7 @@ void ProcessPeriod(void)
     uint8_t flag = 0u;
     uint16_t max_peak = 0u;
     uint8_t period_clear = Curr_Period + 1u;
-    
+
     // Increment every period. When it reaches 10, learning restarts.
     Song_Off++;
 
@@ -296,7 +213,7 @@ void ProcessPeriod(void)
     {
         Max_Peaks[oldest_period][0u] = max_peak;
         Max_Peaks[oldest_period][1u] = Curr_Period;
-            
+
         uint16_t min_peak = Max_Peaks[0u][0u];
         for(i = 0; i < NUM_STORED_PEAKS; i++)
         {
@@ -308,7 +225,7 @@ void ProcessPeriod(void)
 
         Max_Comp_Peak = min_peak;
     }
-    
+
     // If the song has been off for longer than 2.25 seconds, clear the peaks.
     if(Song_Off >= 3u)
     {
@@ -318,10 +235,10 @@ void ProcessPeriod(void)
             Max_Peaks[i][1u] = 0u;
         }
     }
-    
+
     Peak_Count = 0u;
     T2_Rollover = 0u;
-    
+
     Curr_Period++;
     if(Curr_Period == NUM_STORED_PERIODS)
     {
@@ -354,11 +271,11 @@ void SenseBeats(void)
         {
             prev_adc_val = adc_val;
             adc_val = Peripherals_ADC_Convert();
-        } while((prev_adc_val < adc_val) || 
-                ((Org_Adc_Val > prev_adc_val) && 
-                ((Org_Adc_Val - prev_adc_val) < ADC_QTR_VOLT) && 
+        } while((prev_adc_val < adc_val) ||
+                ((Org_Adc_Val > prev_adc_val) &&
+                ((Org_Adc_Val - prev_adc_val) < ADC_QTR_VOLT) &&
                 (prev_adc_val - adc_val < ADC_EIGHTH_VOLT)));
-        
+
         if((Org_Adc_Val > prev_adc_val) && (Org_Adc_Val - prev_adc_val < ADC_QTR_VOLT))
         {
             prev_adc_val = Org_Adc_Val;
@@ -366,7 +283,7 @@ void SenseBeats(void)
         // Set to the highest found value
         Peaks[Curr_Period][Peak_Count] = prev_adc_val;
         Peak_Count++;
-        
+
         Led_Count += 100u;
         ChooseColor();
 
@@ -374,7 +291,7 @@ void SenseBeats(void)
         uint16_t timer_val = TMR2;
         TMR2 = 0u;
         while(TMR2 < (150 * T2_ONE_MS));
-        
+
         if(((timer_val + 150 * T2_ONE_MS) > THREE_QUARTERS_SEC_PERIOD) || (Peak_Count == PEAKS_PER_PERIOD))
         {
             IFS0bits.T2IF = 1u;  // Set interrupt flag as the timer would have triggered an interrupt
@@ -385,12 +302,12 @@ void SenseBeats(void)
             TMR2 = timer_val + (150 * T2_ONE_MS);
         }
     }
-    else if((adc_val > (ADC_HALF_VOLT + ADC_QTR_VOLT)) && 
+    else if((adc_val > (ADC_HALF_VOLT + ADC_QTR_VOLT)) &&
             (adc_val < (ADC_TWO_VOLT + ADC_HALF_VOLT)))
     {
         Song_Off = 0u;
     }
-    
+
     if(FADE_WAIT < (TMR2 - Fade_Time))
     {
         Fade_Time = TMR2;
@@ -398,7 +315,7 @@ void SenseBeats(void)
     }
 }
 
-/** 
+/**
  * Keeps the same color ratio, just dims the LEDS.
  */
 void FadeColor(void)
@@ -410,67 +327,17 @@ void FadeColor(void)
             Curr_Color.Red = Curr_Color.Red * 0.98;
             Curr_Color.Grn = Curr_Color.Grn * 0.98;
             Curr_Color.Blu = Curr_Color.Blu * 0.98;
-            
+
             SetColor();
         }
     }
-}
-
-/**
-    Find two peaks within 3/4 of a second of each other.
-*/
-void SenseClaps(void)
-{
-    Timer_State = SAME_PERIOD;
-    TMR2 = 0u;
-    // Search for a peak
-    FindPeak(LOW_THRESH, CLAP_THRESH);
-
-    // Wait for at least 30ms to ensure the original signal isn't re-sampled
-    TMR2 = 0u;
-    while(TMR2 < (30 * T2_ONE_MS));
-    TMR2 = 0u;
-
-    // Search for a peak
-    FindPeak(LOW_THRESH, CLAP_THRESH);
-
-    // If another peak is found within the same time period, increment the clap count
-    if(SAME_PERIOD == Timer_State)
-    {
-        Clap_Count++;
-    }
-}
-
-/*
-    Find a voltage peak. This signifies a beat or a clap.
-
-    @param low_thresh All voltages below this are considered lows.
-    @param high_thresh All voltages above this are considered peaks.
-*/
-void FindPeak(uint16_t low_thresh, uint16_t high_thresh)
-{
-    uint16_t adc_val = 0u;
-
-    // Wait for a high.
-    // If in beat state or a T2 interrupt has occurred, exit.
-    do{
-        // Sample until a measurement of interest occurs
-        adc_val = Peripherals_ADC_Convert();
-    } while((adc_val < high_thresh) && (Current_State == CLAP_STATE) && 
-            (Timer_State == SAME_PERIOD));
-
-    // Wait for a low
-    do{
-        adc_val = Peripherals_ADC_Convert();
-    } while((adc_val > low_thresh) && (Current_State == CLAP_STATE) &&
-            (Timer_State == SAME_PERIOD));
 }
 
 /*
     Sets the color of the LED strip depending on the size of the peak.
 */
 void ChooseColor(void)
-{    
+{
     if(Led_Count < 100u)
     {
         Org_Color.Red = 100u;
@@ -537,11 +404,11 @@ void ChooseColor(void)
         Org_Color.Grn = 100u;
         Org_Color.Blu = 100u;
     }
-    
+
     Curr_Color.Red = Org_Color.Red;
     Curr_Color.Grn = Org_Color.Grn;
     Curr_Color.Blu = Org_Color.Blu;
-    
+
     SetColor();
 }
 
